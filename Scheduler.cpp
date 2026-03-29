126e3eb70588447854b2dd23703f0022de37afde
Your Name <zhao.henry14@gmail.com>
2026-03-29 15:26:58 -0500

    helpers

--- Scheduler.cpp
+++ Scheduler.cpp
@@ -7,8 +7,10 @@
 
 #include "Scheduler.hpp"
 
+uint64_t num_tasks;
 
 void Scheduler::Init() {
+    num_tasks = GetNumTasks();
     unsigned total = Machine_GetTotal();
     machines_by_cpu.reserve(4);
     machine_states.reserve(total);
@@ -81,6 +83,85 @@ void Scheduler::TaskComplete(Time_t now, TaskId_t task_id) {
     SimOutput("Scheduler::TaskComplete(): Task " + std::to_string(task_id) + " is complete at " + std::to_string(now), 4);
 }
 
+std::optional<MachineId_t> Scheduler::getAvailableMachine(CPUType_t cpu) {
+    unsigned best_mem = 0;
+    std::optional<MachineId_t> best;
+
+    auto it = machines_by_cpu.find(cpu);
+    if (it == machines_by_cpu.end()) return std::nullopt;
+
+    for (MachineId_t mid : it->second) {
+        if (machine_states[mid] != MachineState_t::S0) continue;
+
+        MachineInfo_t info = Machine_GetInfo(mid);
+
+        unsigned threshold = info.memory_size * 8 / 10;
+        if (info.memory_used >= threshold) continue;
+
+        if (info.memory_used > best_mem) {
+            best_mem = info.memory_used;
+            best = mid;
+        }
+    }
+
+    return best;
+}
+
+VMId_t Scheduler::getOrCreateVM(MachineId_t machine, VMType_t vm_type, CPUType_t cpu) {
+    // try to find
+    auto it = vms_on_machine.find(machine);
+    if (it != vms_on_machine.end()) {
+        for (VMId_t vm : it->second) {
+            // reuse if can
+            if (vm_types[vm] == vm_type && migrating_vms.count(vm) == 0) {
+                return vm;
+            }
+        }
+    }
+
+    // new one
+    VMId_t vm = VM_Create(vm_type, cpu);
+    VM_Attach(vm, machine);
+    vms_on_machine[machine].push_back(vm);
+    vm_types[vm] = vm_type;
+
+    return vm;
+}
+
+void Scheduler::wakeMachine(MachineId_t machine) {
+    if (waking_machines.count(machine)) return;
+    
+    Machine_SetState(machine, MachineState_t::S0);
+    machine_states[machine] = MachineState_t::S4;
+    waking_machines.insert(machine);
+}
+
+void Scheduler::StateChangeComplete(Time_t time, MachineId_t machine_id) {
+    machine_states[machine_id] = MachineState_t::S0;
+    waking_machines.erase(machine_id);
+
+    // flush
+    auto it = pending_tasks.find(machine_id);
+    if (it == pending_tasks.end()) return;
+
+    for (TaskId_t task_id : it->second) {
+        TaskInfo_t info = GetTaskInfo(task_id);
+        // check if task completed while we were running this
+        if (info.completed) continue;
+        VMId_t vm = getOrCreateVM(machine_id, info.required_vm, info.required_cpu);
+        VM_AddTask(vm, task_id, getPriority(info.required_sla));
+    }
+    pending_tasks.erase(it);
+}
+
+Priority_t Scheduler::getPriority(SLAType_t sla) {
+    switch(sla) {
+        case SLAType_t::SLA0: return Priority_t::HIGH_PRIORITY;
+        case SLAType_t::SLA1: return Priority_t::MID_PRIORITY;
+        default:              return Priority_t::LOW_PRIORITY;
+    }
+}
+
 // Public interface below
 
 static Scheduler Scheduler;
@@ -137,6 +218,6 @@ void SLAWarning(Time_t time, TaskId_t task_id) {
 }
 
 void StateChangeComplete(Time_t time, MachineId_t machine_id) {
-    // Called in response to an earlier request to change the state of a machine
+    Scheduler.StateChangeComplete(time, machine_id);
 }
 
